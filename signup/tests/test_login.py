2233c492028effc7036143c116ce46187e658551
RobM <robpmacgregor@gmail.com>
2018-11-15 21:46:21 +0000

    adding login tests

---
+++ signup/tests/test_login.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from django.urls import reverse
+
+# Create your tests here.
+
+class LoginTest(TestCase):
+
+    fixtures = ['users.json']
+
+    TEST_USERNAME = "testuser_1"
+    TEST_PASSWORD = "testuser_1_pw"
+
+    def test_login_redirects_if_authenticated(self):
+        self.client.login(username=self.TEST_USERNAME, password=self.TEST_PASSWORD)
+        response = self.client.get(reverse('login'))
+        self.assertEquals(response.status_code, 302)
+
+    def test_nav_content_does_not_display_if_unauthenticated(self):
+        response = self.client.get(reverse('login'))
+        self.assertNotContains(response,'<ul class="navbar-nav mr-auto">')
+
+    def test_login_form_displays_if_unauthenticated(self):
+        response = self.client.get(reverse('login'))
+        self.assertContains(response, '<input type="text" name="username"')
+        self.assertContains(response, '<input type="password" name="password"')
+        self.assertContains(response, '<button type="submit">Login</button>')
